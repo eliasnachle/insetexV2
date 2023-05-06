e1d8021e2bda66de2e6fd5ed04eeb4efec04d699
eliasnachle <elias.nachle@bandtec.com.br>
2023-05-06 02:44:45 -0300

    feat: add responsive footer

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/presentation/components/footer/footer";
 import Header from "@/presentation/components/header/header";
 import About from "@/presentation/components/home/about/about";
-import Contact from "@/presentation/components/home/contact/contact";
 import Hero from "@/presentation/components/home/hero/hero";
 import Service from "@/presentation/components/home/service/service";
 
@@ -12,7 +11,7 @@ export default function Home() {
       <Hero />
       <Service />
       <About />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
--- src/presentation/components/footer/footer.css.ts
+++ src/presentation/components/footer/footer.css.ts
@@ -1,37 +1,44 @@
-import { globalStyle, style } from '@vanilla-extract/css';
-import { vars } from '../../styles/theme/theme.css'
+import { globalStyle, style } from "@vanilla-extract/css";
+import { vars } from "../../styles/theme/theme.css";
 
-export const footerContainer = style({   
-    fontFamily: 'DM Sans', 
-    background: vars.color.backgroundBlack,
-    color: vars.color.lightText,
-    display: 'grid',
-    placeItems: 'center',
+export const footerContainer = style({
+  fontFamily: "DM Sans",
+  background: vars.color.backgroundBlack,
+  color: vars.color.lightText,
+  display: "grid",
+  placeItems: "center",
 });
 
-export const footerContent = style({    
-    maxWidth: vars.sizes.desktopWidth,
-    width: '100%'
+export const footerContent = style({
+  maxWidth: vars.sizes.desktopWidth,
+  width: "100%",
+  padding: "0 1em",
 });
 
 export const footerGrid = style({
-    display: 'flex',
-    justifyContent: 'space-between',
-    gap: '.5rem',
-    padding: '45px 0'
+  display: "flex",
+  justifyContent: "space-between",
+  gap: ".5rem",
+  padding: "45px 0",
+  "@media": {
+    "screen and (max-width: 920px)": {
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center'
+    },
+  },
 });
 
 globalStyle(`${footerGrid} > article`, {
-    width: '300px'
+  width: "auto",  
 });
 
 globalStyle(`${footerGrid} > article > svg`, {
-    width: '200px'
+  width: "200px",
 });
 
-export const footerCopyrigth = style({    
-    padding: '20px 0',
-    fontSize: '14px',
-    color: vars.color.grayText
+export const footerCopyrigth = style({
+  padding: "20px 0",
+  fontSize: "14px",
+  color: vars.color.grayText,
 });
-
