1222b8171daf242041fc7ca545b58a9d35a5f25a
eliasnachle <elias.nachle@bandtec.com.br>
2024-05-17 21:16:31 -0300

    refactor: separate responsibilities

--- src/application/budget/controlStep/controlStep.tsx
+++ src/application/budget/controlStep/controlStep.tsx
@@ -1,49 +1,41 @@
 import { Steps } from '@/domain/types/budget/budgetEnum'
 import { BudgetState } from '@/domain/types/budget/budgetTypes'
-import { sendBudgetMail } from '@/useCases/budgetUseCases'
+import { sendBudgetMail, createValidationStep, incrementStepAndScrollToTop } from '@/useCases/budgetUseCases'
 import { scrollToTop } from '@/utils/utils'
 import { Component } from 'react'
 import { containerControlStep, previousButton } from './controlStep.css'
-import ValidateAddressStep from './validateStep/validateAddressStep'
-import ValidateServicesStep from './validateStep/validateServiceStep'
-import ValidateStep from './validateStep/validateStep'
-import ValidateUserStep from './validateStep/validateUserStep'
 
-interface IStep {
+interface IStepProps {
   state: BudgetState
   handleSetStep: (newStep: number) => void
   handleInputChange: <K extends keyof BudgetState>(key: K, value: BudgetState[K]) => void
 }
 
-const validationSteps: { [step in Steps]: ValidateStep } = {
-  [Steps.STEP_SERVICES]: new ValidateServicesStep(),
-  [Steps.STEP_ADDRESS]: new ValidateAddressStep(),
-  [Steps.STEP_USER]: new ValidateUserStep(),
-  [Steps.STEP_SUCCESS]: new ValidateUserStep(),
-}
-
-export default class ControlStep extends Component<IStep> {  
+export default class ControlStep extends Component<IStepProps> {
   render() {
-    const { state, handleSetStep, handleInputChange } = this.props
+    const { state, handleInputChange, handleSetStep } = this.props
 
-    if (state.step === Steps.STEP_SUCCESS) return null
-
-    const handleNextStepClick = async () => {      
-      const validationFunction = validationSteps[state.step as Steps]
-      if (validationFunction && validationFunction.validate(state, handleInputChange))          
-      if(state.step != Steps.STEP_USER) {
-        handleSetStep(state.step + 1)
-        scrollToTop()      
-      }            
-      await handleSuccessStep(state, handleSetStep)
+    const handleNextStepClick = async () => {
+      const validationStep = createValidationStep(state.step as Steps)
+      if (validationStep.validate(state, handleInputChange)) {
+        state.step === Steps.STEP_USER 
+        ? await handleSuccessStep(state, handleSetStep)
+        : incrementStepAndScrollToTop(state, handleSetStep)        
+      }
     }
 
     const handlePrevStep = () => {
       handleSetStep(state.step - 1)
       scrollToTop()
-    }    
+    }
 
-    return (      
+    async function handleSuccessStep(state: BudgetState, handleSetStep: (newStep: Steps) => void) {
+      const response = await sendBudgetMail(state)
+      if (response) incrementStepAndScrollToTop(state, handleSetStep)
+    }
+
+    if (state.step === Steps.STEP_SUCCESS) return null
+    return (
       <div
         className={containerControlStep}
         style={{ justifyContent: state.step >= Steps.STEP_ADDRESS ? 'space-between' : 'end' }}>
@@ -51,18 +43,9 @@ export default class ControlStep extends Component<IStep> {
           <button className={previousButton} onClick={handlePrevStep}>
             Voltar
           </button>
-        )}        
+        )}
         <button onClick={handleNextStepClick}>Continuar</button>
       </div>
     )
-  }
-}
-
-
-const handleSuccessStep = async (state: BudgetState, handleSetStep: (newStep: number) => void) => {  
-  const response = await sendBudgetMail(state)
-  if(response) {
-    handleSetStep(state.step + 1)
-    scrollToTop()
   }
 }
--- src/useCases/budgetUseCases.tsx
+++ src/useCases/budgetUseCases.tsx
@@ -1,4 +1,10 @@
-import { BudgetState } from '@/domain/types/budget/budgetTypes'
+import ValidateAddressStep from '@/application/budget/controlStep/validateStep/validateAddressStep'
+import ValidateServicesStep from '@/application/budget/controlStep/validateStep/validateServiceStep'
+import ValidateStep from '@/application/budget/controlStep/validateStep/validateStep'
+import ValidateUserStep from '@/application/budget/controlStep/validateStep/validateUserStep'
+import { Steps } from '@/domain/types/budget/budgetEnum'
+import { BudgetState, ValidationResult } from '@/domain/types/budget/budgetTypes'
+import { scrollToTop } from '@/utils/utils'
 import axios from 'axios'
 
 export const sendBudgetMail = async (budgetData: BudgetState): Promise<boolean> => {
@@ -9,4 +15,22 @@ export const sendBudgetMail = async (budgetData: BudgetState): Promise<boolean> 
     console.error('Erro ao enviar orçamento:', e)
     return false
   }
+}
+
+export const createValidationStep = (step: Steps): ValidateStep => {
+  switch (step) {
+    case Steps.STEP_SERVICES:
+      return new ValidateServicesStep()
+    case Steps.STEP_ADDRESS:
+      return new ValidateAddressStep()
+    case Steps.STEP_USER:
+      return new ValidateUserStep()
+    default:
+      return new ValidateServicesStep()
+  }
+}
+
+export function incrementStepAndScrollToTop(state: BudgetState, handleSetStep: (newStep: Steps) => void) {
+  handleSetStep(state.step + 1)
+  scrollToTop()
 }
